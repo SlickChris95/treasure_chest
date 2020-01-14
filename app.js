3d2bc7e2d0ac14e84c5c7848d74f58d943757123
Chris Torres <torres.christian33@yahoo.com>
2020-01-14 15:47:16 -0800

    added form to NEW route

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 let express = require('express');
 let app = express();
-let bodyParser = require('body-parser');
-let mongoose = require('mongoose');
+// let bodyParser = require('body-parser');
+// let mongoose = require('mongoose');
 let port = 3000;
 
 
-/* INDEX ROUTE */
+/* APP CONFIG */
+// mongoose.connect("mongodb://localhost/treasure_chest");
+app.set('view engine','ejs')
+// app.use(bodyParser.urlencoded({extended:true}));
+
+
 app.get('/',(req,res)=>{
-  res.send('Index');
-})
+  res.render('dashboard');
+});
+
+/*
+INDEX ROUTE
+*/
+app.get('/months',(req,res)=>{
+  res.send('Months page');
+});
+
+app.get('/months/new',(req,res)=>{
+  res.render('new');
+});
 
 app.listen(port,()=>{
   console.log('server is running...');
